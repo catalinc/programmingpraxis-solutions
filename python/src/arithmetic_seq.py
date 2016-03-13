a97437172157215c3d60b314acd5a84dc49c87fc
Catalin Cristu <catalin.cristu@gmail.com>
2016-03-13 21:17:22 +0200

    solution to Arithmetic Sequence

--- python/src/arithmetic_seq.py
+++ python/src/arithmetic_seq.py
@@ -16,12 +16,16 @@ def is_arithmetic_seq_fast(l):
     m = min(l)
     s = sum(l)
     n = len(l)
+    # S = X_min + (X_min + diff) + (X_min + 2*diff) + ...
+    #   = N * X_min + diff * (N * (N - 1) / 2)
+    # so (S - X_min) mod (N * (N - 1) / 2) should be 0
     return (s - n * m) % ((n * (n - 1)) / 2) == 0
 
 
 if __name__ == '__main__':
-    test_data = [[1, 2, 3, 4, 5],
-                 [3, 2, 5, 1, 4],
-                 [1, 2, 4, 8, 16]]
+    test_data = [[3, 2, 5, 1, 4],
+                 [1, 2, 4, 8, 16],
+                 [2, 8, 3],
+                 [1, 1, 1, 1]]
     for l in test_data:
         print(is_arithmetic_seq_fast(l))
