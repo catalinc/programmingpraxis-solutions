85152c2456ed23cd3297e5ccf1225b9b41d2ce48
Catalin Cristu <catalin.cristu@gmail.com>
2016-07-05 15:26:21 +0300

    add a better solution to Curious Numbers problem

--- fsharp/curious_numbers.fsx
+++ fsharp/curious_numbers.fsx
@@ -1,18 +1,28 @@
-// Brute force solution to https://programmingpraxis.com/2016/06/28/curious-numbers/
-// TODO: find a faster algorithm
+// Solution to https://programmingpraxis.com/2016/06/28/curious-numbers/
 
 open System
 
 let scale x =
-    let powersOfTen = Seq.unfold (fun p -> Some(p, p * 10I)) 1I
-    powersOfTen |> Seq.skipWhile (fun p -> p < x) |> Seq.head
+    let powersOfTen = Seq.unfold (fun n -> Some(n, n * 10I)) 1I
+    powersOfTen |> Seq.skipWhile (fun n -> n < x) |> Seq.head
 
-let isCurious x =
-    let p = x * x
-    let r = p % scale x
-    r = x
+let isCurious n =
+    let p = n * n
+    let r = p % scale n
+    r = n
 
-let solve x =
-    [1I .. x] |> Seq.filter isCurious |> Seq.sum;
+let getNextCurious c =
+    let s = scale c
+    let ns = Seq.unfold (fun n -> Some(n, n + 1I)) 1I
+    ns |> Seq.map (fun n -> n * s + c) |> Seq.filter (fun n -> isCurious n) |> Seq.head
 
-printfn "%A" (solve 10I**3)
+let curious c = Seq.unfold(fun n -> Some(n, getNextCurious n)) c
+
+let sumCurious x n =
+    curious x |> Seq.takeWhile (fun c -> c < n) |> Seq.sum 
+
+let solve n =
+    let r = [5I..6I] |> List.map (fun x -> sumCurious x n) |> List.sum
+    r + 1I
+
+printfn "sum: %A" (solve (10I**20))
